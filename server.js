a3e54b5c71417498fc5749d07907da2493bc797c
Pablo Sanfilippo <sanfilippopablo@gmail.com>
2014-06-05 10:19:14 -0300

    Some organization in server.js.

--- server.js
+++ server.js
@@ -10,7 +10,7 @@
 //Seteo la variable environment
 var environment = 'development';
 
-/* Cargo dependencias necesarias */
+// Dependencias
 var config      = require('./app/config/config.js')[environment];
 var express     = require('express');
 var mongoose    = require('mongoose');
@@ -18,43 +18,38 @@ var passport    = require('passport');
 var errorHandling = require('./app/config/errorHandling');
 var bodyParser = require('body-parser');
 
-//Inicializo el objeto App
-var app         = express();
+var app = express();
 
 
 
-//Ejecuto los scripts necesarios
+// Configuración
 require('./app/config/mongoose.js')
 	(config, mongoose);
 
 require('./app/config/passport.js')
 	(config, passport);
 
-// Start lo que solia ser express.js
+
 app.use(bodyParser());
 
+// Render engine. Pronto a volar.
 app.set('view engine', 'ejs');
-
 app.set('views', config.rootPath + '/public/app/views');
-
 app.set('view options', { layout:false, root: config.rootPath + '/public/app/views' } );
-
 app.use(express.static(config.rootPath + '/public'));
 
+// Auth middleware
 app.use(passport.initialize());
-
 var tokenAuthMiddleware = require('./app/authentication/middleware')
 app.use(tokenAuthMiddleware);
 
-//End lo que solia ser express.js
-
+// Routes imports
 require('./app/authentication/routes.js')
 	(config, app, passport);
 
+
 app.use(errorHandling);
 
-//Inicio la aplicacion
+// Run
 app.listen(config.port);
-
-//Muestro estado de la aplicacion por consola
 console.log('Listening at '+ config.port+ " " + environment);
