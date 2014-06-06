079397058bd1b9b715d2b2fe5773d1c6eb05f262
Pablo Sanfilippo <sanfilippopablo@gmail.com>
2014-06-06 20:14:21 -0300

    Agregadas rutas básicas para apuntes.

--- app/apuntes/routes.js
+++ app/apuntes/routes.js
@@ -17,19 +17,61 @@ router.route('/apuntes')
 
   .post(function(request, response, next){
     // Create apunte
+    var apunte = new Apunte(request.body);
+    apunte.save(function(error, apunte){
+      if (!error) {
+        response.json({id: apunte.id});
+      }
+      else {
+        next(error);
+      }
+    })
   });
 
 router.route('/apuntes/:id')
   .get(function(request, response, next){
     // Get apunte
+    Apunte.findById(request.params.id, function(error, apunte){
+      if (error) {
+        return next(error);
+      }
+      if (!apunte) {
+        return response.send(404);
+      }
+      else {
+        response.json(apunte);
+      }
+    })
   })
 
   .put(function(request, response, next){
     // Update apunte
+    Apunte.findByIdAndUpdate(request.params.id, request.body, {}, function(error, apunte){
+      if (error) {
+        return next(error);
+      }
+      if (!apunte) {
+        return response.send(404);
+      }
+      else {
+        response.send(200);
+      }
+    })
   })
 
   .delete(function(request, response, next){
     // Remove apunte
+    Apunte.findByIdAndRemove(request.params.id, function(error, apunte){
+      if (error) {
+        return next(error);
+      }
+      if (!apunte) {
+        return response.send(404);
+      }
+      else {
+        response.send(200);
+      }
+    })
   })
 
 module.exports = router;
