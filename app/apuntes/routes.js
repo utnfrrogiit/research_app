592de8202da52796e65b7d1c99021b139af2c400
Pablo Sanfilippo <sanfilippopablo@gmail.com>
2014-06-05 22:21:24 -0300

    Deleted garbage code.

--- app/apuntes/routes.js
+++ app/apuntes/routes.js
@@ -24,25 +24,4 @@ router.route('/apuntes/:id')
     // Remove apunte
   })
 
-module.exports.list = function (request, response) {
-  console.log("List llamado")
-  var objects = Apunte.find({})
-                      .limit(20)
-                      .exec(function (error, apuntes) {
-    console.log(apuntes);
-    return response.json(apuntes);
-  });
-}
-
-module.exports.create = function (request, response) {
-  console.log('El create llamado');
-  console.log(request.body);
-  Apunte.create(request.body, function(err, apunte){
-    if (!err){
-      response.json(apunte);
-    }
-    response.json(err);
-  });
-}
-
 module.exports = router;
