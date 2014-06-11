e8782b5164b6f3510d1632d27a95ca3a3c6cda3e
Pablo Sanfilippo <sanfilippopablo@gmail.com>
2014-06-11 17:57:20 -0300

    Agregadas las dependencias necesarias para el uso de gulp. Además, gulpfile.js básico con tasks lint y develop (reinicia el server en cambios de archivos).

---
+++ gulpfile.js
@@ -0,0 +1,17 @@
+var gulp = require('gulp');
+var jshint = require('gulp-jshint');
+var nodemon = require('gulp-nodemon');
+
+gulp.task('lint', function() {
+  return gulp.src([
+      './app/**/*.js',
+      './public/**/*.js',
+      '!./public/libs/**/*.js'
+    ])
+    .pipe(jshint())
+    .pipe(jshint.reporter('jshint-stylish'));
+});
+
+gulp.task('develop', function () {
+  nodemon({ script: 'server.js', ext: 'html js css'})
+})
